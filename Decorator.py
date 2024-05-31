fe80a2ab6cd988ae43a0d719afe28a3b1ea88b45
AlexsandrG <167127179+AlexsandrG@users.noreply.github.com>
2024-05-31 11:12:42 +0300

    Add files via upload

--- Decorator.py
+++ Decorator.py
@@ -1,13 +1,21 @@
-def is_prime(func): # Скелет функции декоратор
-    def wrapper():
-        print('Простое')
-        func()
+def is_prime(func):
+    def wrapper(*args):
+        result = func(*args)
+        for i in range(2, result):
+            if result % i == 0:
+                return f'{result} \n Составное'
+            else:
+                return f' {result} \n Простое'
     return wrapper
 
-@is_prime # То что вызывает декоратор
-def sum_three():
-    total = sum(sum_three)
-    print(total)
+
+@is_prime
+def sum_three(*args):
+    total = 0
+    for number in args:
+        total += number
+    return total
+
 
 result = sum_three(2, 3, 6)
 print(result)
